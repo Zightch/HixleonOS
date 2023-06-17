90c5508e5a9055600cf6edeb9cd3b2fc698e50c9
Zightch <Zightch@163.com>
2023-06-18 00:49:07 +0800

    .

--- 006/arch/x86/GDT.cpp
+++ 006/arch/x86/GDT.cpp
@@ -5,16 +5,16 @@
 unsigned long long gdt_[GDT_ENTRY];
 unsigned short gdtLimit_ = sizeof(gdt_);
 
-void setGDTEntry(unsigned int index, unsigned int base, unsigned int limit, unsigned int flags) {
+void setGDT(unsigned int index, unsigned int base, unsigned int limit, unsigned int flags) {
     gdt_[index] = SEG_BASE_H(base) | flags | SEG_LIM_H(limit) | SEG_BASE_M(base);
     gdt_[index] <<= 32;
     gdt_[index] |= SEG_BASE_L(base) | SEG_LIM_L(limit);
 }
 
 void initGDT_() {
-    setGDTEntry(0, 0, 0, 0);
-    setGDTEntry(1, 0, 0xFFFFF, SEG_R0_CODE);
-    setGDTEntry(2, 0, 0xFFFFF, SEG_R0_DATA);
-    setGDTEntry(3, 0, 0xFFFFF, SEG_R3_CODE);
-    setGDTEntry(4, 0, 0xFFFFF, SEG_R3_DATA);
+    setGDT(0, 0, 0, 0);
+    setGDT(1, 0, 0xFFFFF, SEG_R0_CODE);
+    setGDT(2, 0, 0xFFFFF, SEG_R0_DATA);
+    setGDT(3, 0, 0xFFFFF, SEG_R3_CODE);
+    setGDT(4, 0, 0xFFFFF, SEG_R3_DATA);
 }

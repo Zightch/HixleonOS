13c2e41052752afb1a598ae0ef3b282049d758c7
zightch <zightch@163.com>
2024-10-26 21:16:14 +0800

    完全完成grub引导

--- package-grub-img.sh
+++ package-grub-img.sh
@@ -5,21 +5,15 @@ rm -rf HixleonOS-grub.img
 dd if=/dev/zero of=HixleonOS-grub.img bs=256M count=1
 
 fdisk HixleonOS-grub.img << EOF
-g
+o
 n
-
-
-+64MB
-n
-
-
-
-t
+p
 1
-4
+
+
+a
 t
-2
-20
+83
 p
 w
 EOF
@@ -27,14 +21,14 @@ EOF
 sudo losetup /dev/loop7 HixleonOS-grub.img
 sudo kpartx -av /dev/loop7
 
-sudo mkfs.ext4 /dev/mapper/loop7p1
-sudo mkfs.ext4 /dev/mapper/loop7p2
+sudo mkfs.ext2 /dev/mapper/loop7p1
 mkdir tmp
 sudo mount /dev/mapper/loop7p1 tmp
 
-sudo grub-install --boot-directory=tmp/boot --no-floppy --target=i386-pc --force /dev/loop7
+sudo grub-install --boot-directory=tmp/boot --no-floppy --target=i386-pc /dev/loop7
 
-sudo cp boot/grub/grub.cfg tmp/boot/grub/grub.cfg
+sudo cp boot/grub/grub.cfg tmp/boot/grub
+sudo cp kernel/KERNEL tmp
 
 sudo umount -R tmp
 rm -rf tmp
